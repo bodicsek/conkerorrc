5443b317047a0aa73ca9ef71f03a80e157dbaebb
bodicsek <david.nabraczky@gmail.com>
2014-12-21 11:00:03 +0100

    google search next-page previous-page link keyboard bindings

--- google-search_results.js
+++ google-search_results.js
@@ -1,2 +1,7 @@
 require("google-search-results");
 google_search_bind_number_shortcuts();
+
+// Bind the "n" key to the next page link, the "p" key to the previous-page link
+// note: works only if the display language is english
+define_key(google_search_results_keymap, "n", "follow", $browser_object = browser_object_relationship_next);
+define_key(google_search_results_keymap, "p", "follow", $browser_object = browser_object_relationship_previous);
